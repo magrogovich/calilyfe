fe451dfaf4d2a421c6a6558fe80549e2a0a83499
user <user@users-MBP.home>
2022-07-10 20:57:33 +0100

    update8

---
+++ log.js
@@ -0,0 +1,7 @@
+function verif(){
+    name = log.name.value
+    pass = log.pass.value
+
+    rem = log.rem.checked
+
+}
---
+++ shop.js
@@ -0,0 +1,35 @@
+// const slider = document.querySelector('.slider')
+// const imgs = document.querySelectorAll('.slider img')
+
+
+// const pervbtn = document.querySelector("#pervbtn")
+// const nextbtn = document.querySelector("#nextbtn")
+
+// let counter = 1
+// const size = imgs[0].clientWidth
+// slider.style.transform = 'translateX(' + (-size*counter)+'px)';
+
+
+// nextbtn.addEventListener('click',()=>{
+//     slider.style.transition = "transform 0.4s ease-in-out"
+//     counter++; 
+//     console.log(counter);
+//     slider.style.transform = 'translateX(' + (-size*counter)+'px)';
+// })
+
+// pervbtn.addEventListener('click',()=>{
+//     slider.style.transition = "transform 0.4s ease-in-out"
+//     counter--; 
+//     console.log(counter);
+//     slider.style.transform = 'translateX(' + (-size*counter)+'px)';
+// })
+
+// slider.addEventListener('transtionend',()=>{
+//     console.log(imgs[counter]);
+//     if(imgs[counter].id === 'lastclone'){
+//         slider.style.transition = "none";
+//         counter = imgs.length - 2;
+//         slider.style.transform = 'translateX(' + (-size*counter)+'px)';
+//     }
+// })
+
